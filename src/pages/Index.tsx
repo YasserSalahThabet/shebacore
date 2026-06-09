fee1bfb57393e705308fcc5066d958b6f5b117e5
Yasser Thabet <101029259+YasserSalahThabet@users.noreply.github.com>
2026-06-09 19:30:01 +0300

    Declutter homepage section stack

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,10 +2,8 @@ import { Layout } from "@/components/layout/Layout";
 import { HeroSection } from "@/components/home/HeroSection";
 import { StatsSection } from "@/components/home/StatsSection";
 import { AIProcessSection } from "@/components/home/AIProcessSection";
-import { WhatWeDoSection } from "@/components/home/WhatWeDoSection";
 import { SolutionsSection } from "@/components/home/SolutionsSection";
 import { WhyShebaCore } from "@/components/home/WhyShebaCore";
-import { WhoWeServeSection } from "@/components/home/WhoWeServeSection";
 import { CTASection } from "@/components/home/CTASection";
 
 const Index = () => {
@@ -14,10 +12,8 @@ const Index = () => {
       <HeroSection />
       <StatsSection />
       <AIProcessSection />
-      <WhatWeDoSection />
       <SolutionsSection />
       <WhyShebaCore />
-      <WhoWeServeSection />
       <CTASection />
     </Layout>
   );
